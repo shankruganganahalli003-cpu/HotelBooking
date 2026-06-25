ec3f1a092c97366f568210a6a13f6f12b16bd736
shankruganganahalli003-cpu <shankruganganahalli003@gmail.com>
2026-06-25 15:21:15 +0530

    fix root route

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://hotelbooking-g7sq.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true
 }));
 
