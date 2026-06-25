e46c8cc3de1acfa180b38a8c57519545083b5c3e
shankruganganahalli003-cpu <shankruganganahalli003@gmail.com>
2026-06-25 16:38:03 +0530

    fix root route

--- client/src/Admin/UpdateRoom.jsx
+++ client/src/Admin/UpdateRoom.jsx
@@ -40,7 +40,7 @@ const UpdateRoom = () => {
     e.preventDefault();
     try {
       const { data } = await axios.put(
-        `http://localhost:3000/api/room/update/${id}`,
+        `https://unidhuhkdccbdkj.onrender.com/api/room/update/${id}`,
         form,
         { headers: { "Content-Type": "application/json" }, withCredentials: true }
       );
